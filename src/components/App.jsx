3670baba8fdb4a7c75b2f699d3ff38dae032dfb2
SJanna <jannas@uninorte.edu.co>
2023-09-12 05:00:13 -0400

    Taller 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,86 +1,64 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CircularPhoto from "./CircularPhoto";
 import HeaderText from "./HeaderText";
 import IconList from "./IconList";
 import "./styles/App.css";
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
+const App =()=> {
+const [data, setData] = useState(null);
+const [state, setState] = useState({});
 
-    this.state = {
-      isActive: "profile",
-      text:"My name is",
-      data:"Shalem Janna",
-    };
-  }
-  profile = {
-    text: "My name is",
-    data: "Shalem Janna",
-  };
-  mail = {
-    text: "My email address is",
-    data: "sjannadiaz@gmail.com",
-  };
-  calendar = {
-    text: "My birthday is",
-    data: "July XX, 2XXX",
-  };
-  map = {
-    text: "My address is",
-    data: "Calle XXb #XX -XXX",
-  };
-  phone = {
-    text: "My phone number is",
-    data: "301 XXX XXXX",
-  };
-  lock = {
-    text: "My credentials are",
-    data: "*********",
-  };
-  handleChange = (e) => {
-    console.log(e.currentTarget.name);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.results[0]);
+        setState({isActive: "profile", text: "My name is", data: data.results[0].name.first + " " + data.results[0].name.last});
+      }
+    );
+  }, []);
+
+
+ const handleChange = (e) => {
     if (e.currentTarget.name === "profile"){
-      this.setState({isActive: "profile", text: this.profile.text, data: this.profile.data});
+      setState({isActive: "profile", text: "My name is", data: data.name.first + " " + data.name.last});
     }
     else if (e.currentTarget.name === "mail"){
-      this.setState({isActive: "mail", text: this.mail.text, data: this.mail.data});
+      setState({isActive: "mail", text: "My email address is", data: data.email});
     }
     else if (e.currentTarget.name === "calendar"){
-      this.setState({isActive: "calendar", text: this.calendar.text, data: this.calendar.data});
+      setState({isActive: "calendar", text: "My date of birth is", data: data.dob.date});
     }
     else if (e.currentTarget.name === "map"){
-      this.setState({isActive: "map", text: this.map.text, data: this.map.data});
+      setState({isActive: "map", text: "I'm from", data: data.location.city + ", " + data.location.state + ", " + data.location.country});
     }
     else if (e.currentTarget.name === "phone"){
-      this.setState({isActive: "phone", text: this.phone.text, data: this.phone.data});
+      setState({isActive: "phone", text: "My phone number is", data: data.phone });
     }
     else if (e.currentTarget.name === "lock"){
-      this.setState({isActive: "lock", text: this.lock.text, data: this.lock.data});
+      setState({isActive: "lock", text: "My username is", data: data.login.username});
     }
   };
 
-  render() {
     return (
       <div className="main-container">
         <div className="Layout-header">
           <div className="photo-container">
             <CircularPhoto
-              src="https://www.w3schools.com/howto/img_avatar.png"
+              src={data?.picture.large}
               alt="Avatar"
             />
           </div>
         </div>
         <div className="Layout-main">
           <div className="Layout-main-header">
-            <HeaderText info={this.state}/>
+            <HeaderText info={state}/>
           </div>
           <div className="Layout-main-content">
-            <IconList onChange={this.handleChange} info={this.state}/>
+            <IconList onChange={handleChange} info={state}/>
           </div>
         </div>
       </div>
     );
   }
-}
 export default App;
